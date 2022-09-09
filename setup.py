5c68f1bc0aeda0d6f2d2323ccb67552b5bf0a866
Maja Jabłońska <majajjablonska@gmail.com>
2022-09-09 19:17:31 +0200

    Add setup

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ except ImportError:
     from distutils.core import find_packages, setup
 #from distutils.extension import Extension
 
-setup(name="astromet",
-      version='1.1.4',
-      description='One and two body astrometry',
-      author='Zephyr Penoyre',
-      author_email='zephyrpenoyre@gmail.com',
-      url='https://github.com/zpenoyre/astromet.py',
+setup(name="jaxtromet",
+      version='0.8',
+      description='One and two body astrometry. Created by Zephyr Penoyre and his team, rewritten to JAX by Maja Jabłońska',
+      author='Zephyr Penoyre/rewritten by Maja Jabłońska',
+      author_email='majajjablonska@gmail.com',
+      url='https://github.com/maja-jablonska/jaxtromet.py',
       license='GNU GPLv3',
       packages=find_packages(),
-      install_requires=['numpy','astropy','scipy'],
+      install_requires=['numpy','astropy','scipy','astromet','jax','jaxlib'],
       include_package_data=True,
       package_data={'': ['data/*.csv']},
       )
